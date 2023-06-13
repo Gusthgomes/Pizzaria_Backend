cafce5a13b47ecf6b9435d61667f9d95009f36d4
Gustavo Gomes <heiqueenn@gmail.com>
2023-06-13 11:43:04 -0300

    Create routes of products

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/cotegory/CreateCategoryController';
+import { ListCategoryController } from './controllers/cotegory/ListCategoryController';
+import { CreateProductsController } from './controllers/products/CreateProductsController';
+import uploadConfig from './config/multer';
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 // -- ROUTERS USERS --
 router.post('/users', new CreateUserController().handle)
@@ -16,5 +22,10 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 // ROUTERS CATEGORYS
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+// ROUTERS PRODUCTS
+router.post('/products', isAuthenticated, upload.single('file'),  new CreateProductsController().handle);
+
 
 export{ router };
