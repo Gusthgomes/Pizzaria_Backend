71333c304a4bd71171f9671cd4c8e86f359c1ecf
Gustavo Gomes <heiqueenn@gmail.com>
2023-06-20 09:01:40 -0300

    Create the send, listing and update of orders

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,8 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrderController } from './controllers/order/ListOrderController';
 import uploadConfig from './config/multer';
 
 const router = Router();
@@ -38,6 +40,8 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
 router.post('/order/add', isAuthenticated, new AddItemController().handle);
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
+router.put('/order/send', isAuthenticated, new SendOrderController().handle);
+router.get('/orders', isAuthenticated, new ListOrderController().handle);
 
 export{ router };
 
