0e0bb11eb52c26a11e62c46a3d053271346f423b
Gustavo Gomes <heiqueenn@gmail.com>
2023-06-20 08:28:02 -0300

    Create method delete and post in table Item

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,8 @@ import { CreateProductsController } from './controllers/products/CreateProductsC
 import { ListByCategoryController } from './controllers/products/ListByCategoryController';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
 import uploadConfig from './config/multer';
 
 const router = Router();
@@ -34,6 +36,8 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 //ROUTES ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
-
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 
 export{ router };
+
